b965a609a8d44a83b09989991f36926b56f9cc2e
Ewerton Henrique Marschalk <ewertonmarschalk@gmail.com>
2018-11-26 17:34:14 -0200

    css

--- plugin.php
+++ plugin.php
@@ -20,9 +20,9 @@ $player2placar = $placar[1]->pontos;
         <meta charset="UTF-8">
         <style>
             .enjoy-css {
-                //-webkit-box-sizing: content-box;
-                //-moz-box-sizing: content-box;
-                //box-sizing: content-box;
+                -webkit-box-sizing: content-box;
+                -moz-box-sizing: content-box;
+                box-sizing: content-box;
                 //width: 280px;
                 //cursor: pointer;
                 //padding: 9px 20px;
@@ -30,13 +30,86 @@ $player2placar = $placar[1]->pontos;
                 //border: 1px solid;
                 font: normal 34px/1 "Acme", Helvetica, sans-serif;
                 color: rgba(0,0,0,1);
-                text-align: center;
+                //text-align: center;
                 -o-text-overflow: ellipsis;
+                text-transform: uppercase;
                 text-overflow: ellipsis;
-                //background: #ffffff;
-                //-webkit-box-shadow: 3px 3px 0 0 rgba(0,0,0,0.8) ;
-                //box-shadow: 3px 3px 0 0 rgba(0,0,0,0.8) ;
-                //text-shadow: 1px 1px 1px rgba(0,0,0,0.2) ;
+            }
+            .p1 {
+                -webkit-box-sizing: content-box;
+                -moz-box-sizing: content-box;
+                box-sizing: content-box;
+                width: 200px;
+                height: 25px;
+                top: 7px;
+                left: 20px;
+                cursor: pointer;
+                padding: 10px 0px;
+                overflow: hidden;
+                //border: 1px solid;
+                font: normal 34px/1 "Acme", Helvetica, sans-serif;
+                color: rgba(0,0,0,1);
+                text-align: left;
+                -o-text-overflow: ellipsis;
+                text-transform: uppercase;
+                text-overflow: ellipsis;
+            }
+            .p1p {
+                -webkit-box-sizing: content-box;
+                -moz-box-sizing: content-box;
+                box-sizing: content-box;
+                width: 50px;
+                height: 25px;
+                top: 7px;
+                left: 265px;
+                cursor: pointer;
+                padding: 10px 0px;
+                overflow: hidden;
+                //border: 1px solid;
+                font: normal 34px/1 "Acme", Helvetica, sans-serif;
+                color: rgba(0,0,0,1);
+                text-align: right;
+                -o-text-overflow: ellipsis;
+                text-transform: uppercase;
+                text-overflow: ellipsis;
+            }
+            .p2 {
+                -webkit-box-sizing: content-box;
+                -moz-box-sizing: content-box;
+                box-sizing: content-box;
+                width: 200px;
+                height: 25px;
+                top: 7px;
+                left: 420px;
+                cursor: pointer;
+                padding: 10px 0px;
+                overflow: hidden;
+                //border: 1px solid;
+                font: normal 34px/1 "Acme", Helvetica, sans-serif;
+                color: rgba(0,0,0,1);
+                text-align: right;
+                -o-text-overflow: ellipsis;
+                text-transform: uppercase;
+                text-overflow: ellipsis;
+            }
+            .p2p {
+                -webkit-box-sizing: content-box;
+                -moz-box-sizing: content-box;
+                box-sizing: content-box;
+                width: 50px;
+                height: 25px;
+                top: 7px;
+                left: 333px;
+                cursor: pointer;
+                padding: 10px 0px;
+                overflow: hidden;
+                //border: 1px solid;
+                font: normal 34px/1 "Acme", Helvetica, sans-serif;
+                color: rgba(0,0,0,1);
+                text-align: left;
+                -o-text-overflow: ellipsis;
+                text-transform: uppercase;
+                text-overflow: ellipsis;
             }
         </style>
         <link async href="http://fonts.googleapis.com/css?family=Acme" data-generated="http://enjoycss.com" rel="stylesheet" type="text/css"/>
@@ -45,20 +118,20 @@ $player2placar = $placar[1]->pontos;
     <body>
         <div style='position:relative; top:0px; left:0px; '>
             <img src=img/tema.jpg border=0>
-            <div style='position:absolute; top:15px; left:50px;' class="enjoy-css">
+            <div style='position:absolute; ' class="p1">
                 <?php echo $player1; ?>
             </div>
-            <div style='position:absolute; top:15px; left:480px;' class="enjoy-css">
+            <div style='position:absolute;' class="p2">
                 <?php echo $player2; ?>
             </div>
             <div style='position:absolute; top:15px; left:320px;' class="enjoy-css">
                 |
             </div>
-            <div style='position:absolute; top:15px; left:290px;' class="enjoy-css">
+            <div style='position:absolute;' class="p1p">
                 <?php echo $player1placar; ?>
             </div>
-            <div style='position:absolute; top:15px; left:338px;' class="enjoy-css">
-                <?php echo $player1placar; ?>
+            <div style='position:absolute;' class="p2p">
+                <?php echo $player2placar; ?>
             </div>
         </div>
         
